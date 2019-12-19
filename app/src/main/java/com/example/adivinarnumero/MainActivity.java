e17fa6cdbb97860afa9cadc4d89fa77975fda2e9
David Usón <xldaviduson@gmail.com>
2019-12-19 17:58:03 +0100

    Mejoras

--- app/src/main/java/com/example/adivinarnumero/MainActivity.java
+++ app/src/main/java/com/example/adivinarnumero/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
         final EditText numero = findViewById(R.id.txtNumero);
 
         final TextView txtView = findViewById(R.id.textView);
+        txtView.setMovementMethod(new ScrollingMovementMethod());
+
         final TextView txtView2 = findViewById(R.id.contador);
+
+        final TextView tvPista = findViewById(R.id.tvPista);
 
         // Inicializo el boton con una funcion al pulsarlo:
         final Button button = findViewById(R.id.button);
@@ -37,21 +42,33 @@ public class MainActivity extends AppCompatActivity {
 
                 txtView2.setText("Intentos : " + contador);
 
-                // Recojo el contenido del EditText y lo paso a Int:
-                String StrNumeroRecogido = numero.getText().toString();
-                int numeroRecogido = Integer.valueOf(StrNumeroRecogido);
+                String StrNumeroRecogido = "";
 
                 // Comparo si el numero introducido por el usuario es el mismo que el numero que se ha generado aleatorio:
-                if (numeroRecogido == 41) {
-                    // Se muestra un mensaje conforme has hacertado el numero:
-                    Context context = getApplicationContext();
-                    CharSequence text = "HAS ACERTADO";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                if (numero.equals("") || numero == null) {
+                    Toast t = Toast.makeText(getApplicationContext(), "ERROR: No has introducido ningun número", Toast.LENGTH_SHORT);
+                    t.show();
+                } else {
+                    // Recojo el contenido del EditText y lo paso a Int:
+                    StrNumeroRecogido = numero.getText().toString();
+                    int numeroRecogido = Integer.valueOf(StrNumeroRecogido);
 
-                    // El boton se deshabilita en cuanto el usuario acierta el numero:
-                    button.setEnabled(false);
+                    if (numeroRecogido > randomNum) {
+                        tvPista.setText("El numero es MENOR");
+                    } else if (numeroRecogido < randomNum) {
+                        tvPista.setText("El numero es MAYOR");
+                    } else if (numeroRecogido == randomNum) {
+                        tvPista.setText("");
+                        // Se muestra un mensaje conforme has hacertado el numero:
+                        Context context = getApplicationContext();
+                        CharSequence text = "HAS ACERTADO";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                        // El boton se deshabilita en cuanto el usuario acierta el numero:
+                        button.setEnabled(false);
+                    }
                 }
 
                 // Cada numero que introduzca el usuario se guarda en un texto (primero se recoge el contenido de este y se le añade una linea mas):
